4afe6e2be596c502413d2e60f31d4f82e4b85de8
jiexzhong <jiexunzhong@gmail.com>
2023-10-27 13:00:58 -0700

    added player & computer choice

--- tictactoe.js
+++ tictactoe.js
@@ -1,3 +1,13 @@
+const readline = require('readline-sync');
+
+const INITIAL_MARKER = ' ';
+const PLAYER_MARKER = 'X';
+const COMPUTER_MARKER = 'O';
+
+function prompt(msg) {
+    console.log(`=> ${msg}`);
+}
+
 function displayBoard(board) {
 console.log('');
 console.log('     |     |');
@@ -18,13 +28,41 @@ function initializeBoard() {
     let board = {};
 
     for(let square = 1; square <= 9; square++) {
-        board[String(squre)] = ' ';
+        board[String(square)] = INITIAL_MARKER;
     }
+
     return board;
 }
 
-let board = initializeBoard();
-displayBoard(board)
+function emptySquares(board) {
+    return Object.keys(board).filter(key => board[key] === INITIAL_MARKER);
+}
 
+function playerChoosesSquare(board) {
+    let square;
+    
+    while(true) {
+        prompt(`Choose a square (${emptySquares(board).join(', ')}):`);
+        let square = readline.question().trim();
+        if(emptySquares(board).includes(square)) break; 
+           
+        prompt("That's not a valid choice.")
+    }
+    board[square] = PLAYER_MARKER;
+}
+
+function computerChoosesSquare(board) {
+    let randomIndex = Math.floor(Math.random() * emptySquares(board).length);
+
+    let square = emptySquares(board)[randomIndex];
+    board[square] = COMPUTER_MARKER;
+}
+
+let board = initializeBoard();
+displayBoard(board);
+
+playerChoosesSquare(board);
+computerChoosesSquare(board);
 
 displayBoard(board);
+
